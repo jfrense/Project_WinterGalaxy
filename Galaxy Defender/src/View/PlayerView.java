e301ed268acf96f4f27faa537f379f43efc9a37a
Lsell1 <larry.sell1@gmail.com>
2013-12-19 15:18:07 -0500

    Added Java Files
    Driver, UI, Player, EnemyAlien, Spawner, PlayerView, EnemyView, and
    Background

---
+++ Galaxy Defender/src/View/PlayerView.java
@@ -0,0 +1,48 @@
+package View;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+import Controller.Driver;
+/*******************************************************************************************
+ * 	Title:	Player View
+ * 	Purpose: 	Displays the users character
+ * 
+ *	@author Larry Sell
+ *	@version 0.0.1
+ *******************************************************************************************/
+public class PlayerView extends JComponent {
+	BufferedImage[][] imgs;
+	public int imgNum = 0;
+	public int frameCount = 4;
+	public PlayerView(){
+		BufferedImage[] img = createAnimation();
+		imgs = new BufferedImage[img.length][frameCount];
+		for(int j = 0; j < img.length; j++) {
+			for(int i = 0; i < 4; i++)
+				imgs[j][i] = img[j].getSubimage((128) * i, 0, 128, 100);
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(imgs[0][imgNum],Driver.player.x, Driver.player.y,80,80,null);
+	}
+	
+	//Read images from file and return
+	private BufferedImage[] createAnimation() {
+		BufferedImage[] bufferedImage = new BufferedImage[1];
+		try {
+			bufferedImage[0] = ImageIO.read(new File("images/PlayerWalkRight.png"));
+			return bufferedImage;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
