ff09b73279aa0e4086feb620bb8772c7f64681f4
Justin Frense <jfrense@udel.edu>
2013-12-20 15:17:13 -0500

    Android conversion (still working on it)

---
+++ AndroidGalaxy/src/com/example/androidgalaxy/MainGamePanel.java
@@ -0,0 +1,64 @@
+package com.example.androidgalaxy;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+	private MainThread thread;
+
+	public MainGamePanel(Context context) {
+		super(context);
+
+
+		getHolder().addCallback(this);
+
+		thread = new MainThread(getHolder(), this);
+
+		setFocusable(true);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		boolean retry = true;
+		while(retry){
+			try{
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				// try again to shutdown thread
+			}
+
+		}
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
+		return super.onTouchEvent(event);
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas){
+		
+	
+	
+	}
+
+
+
+}
